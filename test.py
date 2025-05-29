6b0f2edc6a472a9faac49009b052652b443a5809
TheShadowblast123@yahoo.com <TheShadowblast123@yahoo.com>
2025-05-29 15:10:33 -0700

    removed python from project finally

--- test.py
+++
@@ -1,8 +0,0 @@
-tag = "ikimono-gakari_sakura"
-test = "TARGET DECK: Kanji\nSTART\nBasic\n中, 4\nBack: in\n('なか', 'うち', 'あた.る', 'チュウ')\n['｜', '口']\nTags: [[Again_Yui]] \n\nEND"
-lines = test.split('\n')
-for line in lines:
-    if 'Tags: ' in line:
-        lines[lines.index(line)] = line[:-2] + '] ' + f'[[{tag}]] '
-
-print(*lines)
